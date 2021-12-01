30e978897cc22ce6224476f2fb8a77b4338e3681
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-12-01 12:05:46 +0600

    agian color changed

--- src/components/ThemeCustomization.tsx
+++ src/components/ThemeCustomization.tsx
@@ -4,18 +4,33 @@ import { red } from "@mui/material/colors";
 import { Typography } from "@mui/material/";
 
 const ThemeCustomization = () => {
-	const themeColor = createTheme({
+	let theme = createTheme({
 		palette: {
 			error: {
 				main: red[500],
+			},
+			secondary: {
+				main: "#4f4512",
+			},
+		},
+	});
+
+	// change here primary default color
+	theme = createTheme(theme, {
+		palette: {
+			primary: {
+				main: theme.palette.error.main,
 			},
 		},
 	});
 
 	return (
-		<ThemeProvider theme={themeColor}>
-			<Typography variant="h2" color="error">
+		<ThemeProvider theme={theme}>
+			<Typography variant="h2" color="primary">
 				Theme customizations - color
+			</Typography>
+			<Typography component="h2" variant="h5" color="secondary">
+				Hello
 			</Typography>
 		</ThemeProvider>
 	);
